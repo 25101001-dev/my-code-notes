18bd94da8b51d7ea6a3404c9e7aa66dc61e55ce1
Riana Iftekhar <25101001@uap-bd.edu>
2025-11-19 20:40:42 +0600

    Create 1038.c

---
+++ 1038.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    float code[5] = {4.00,4.50,5.00,2.00,1.50};
+    int x,y;
+    scanf("%d %d",&x,&y);
+
+    float price = (float)code[x-1]*y;
+
+    printf("Total: R$ %.2f\n",price);
+
+    return 0;
+}
