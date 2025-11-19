c09a6604cb1863506e4f0b7f38c5fa71133f1ff5
Riana Iftekhar <25101001@uap-bd.edu>
2025-11-19 19:19:30 +0600

    Create 1036.c

---
+++ 1036.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+ 
+    float a,b,c,d,r1,r2;
+    scanf("%f %f %f",&a,&b,&c);
+    
+    d = pow(b,2)-4*a*c;
+    
+    r1 = (-b+sqrt(d))/(2*a);
+    r2 = (-b-sqrt(d))/(2*a);
+    
+    if(a == 0 || d < 0 ){
+        printf("Impossivel calcular\n");
+    }
+    else{
+        printf("R1 = %.5f\n",r1);
+        printf("R2 = %.5f\n",r2);
+    }
+ 
+    return 0;
+}
