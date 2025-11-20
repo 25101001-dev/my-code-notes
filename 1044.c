ad44e838d7c82a8d6845734875713319da64ce9f
Riana Iftekhar <25101001@uap-bd.edu>
2025-11-20 16:42:09 +0600

    Create 1044.c

---
+++ 1044.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int c = b%a;
+    int d = a%b;
+    if(c == 0 || d == 0){
+        printf("Sao Multiplos\n");
+    }
+    else{
+        printf("Nao sao Multiplos\n");
+    }
+ 
+    return 0;
+}
