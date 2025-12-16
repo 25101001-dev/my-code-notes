7d727d514a02fd4d522f14a4f37521c581045948
Riana Iftekhar <25101001@uap-bd.edu>
2025-12-16 18:41:32 +0600

    Implement basic even number adjustment and output

---
+++ 1070.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int a ;
+    cin >> a;
+    if(a%2 == 0) a = a + 1;
+    for(int i=0 ; i < 6 ; i++){
+        cout << a + (2*i) << endl;
+    }
+    return 0;
+}
